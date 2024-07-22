0b95896b36d47ddc4f60a7a6d548b2c4cf33a420
Mivinci <1366723936@qq.com>
2024-07-23 00:42:41 +0800

    :construction: daily

--- misc/2.c
+++ misc/2.c
@@ -1,20 +1,19 @@
 #include <stdlib.h>
 
-extern void go(void *) asm("go");
+extern void go(void *, uintptr_t) asm("go");
 
 static int a;
 
 void goexit(void) {
-  a = 200;
   exit(0);
 }
 
-void f() { a = 100; }
+void f() {}
 
 int main(void) {
   uintptr_t sp = (uintptr_t)malloc(1024);
   sp -= sizeof(void *);
   sp = sp & (~7);
   *(void **)sp = goexit;
-  go(f);
+  go(f, sp);
 }
